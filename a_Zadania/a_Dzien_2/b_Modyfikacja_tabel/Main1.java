05109f862d243ab03cd84b8157db9f251e53a27a
Marek Nibelski <marek.nibelski@gmail.com>
2020-12-17 20:45:53 +0100

    modity tables

--- a_Zadania/a_Dzien_2/b_Modyfikacja_tabel/Main1.java
+++ a_Zadania/a_Dzien_2/b_Modyfikacja_tabel/Main1.java
@@ -3,8 +3,8 @@ package a_Zadania.a_Dzien_2.b_Modyfikacja_tabel;
 
 public class Main1 {
     //zapisz poniżej zapytania do bazy
-    public String query1 = "";
-    public String query2 = "";
-    public String query3 = "";
-    public String query4 = "";
+    public String query1 = "ALTER TABLE tickets ADD priceUSD decimal(7,2)";
+    public String query2 = "ALTER TABLE movies ADD director char(80)";
+    public String query3 = "ALTER TABLE movies MODIFY COLUMN director varchar(50)";
+    public String query4 = "ALTER TABLE tickets DROP COLUMN priceUSD";
 }
--- a_Zadania/a_Dzien_2/b_Modyfikacja_tabel/Main2.java
+++ a_Zadania/a_Dzien_2/b_Modyfikacja_tabel/Main2.java
@@ -3,9 +3,9 @@ package a_Zadania.a_Dzien_2.b_Modyfikacja_tabel;
 
 public class Main2 {
     //zapisz poniżej zapytania do bazy
-    public String query1 = "";
-    public String query2 = "";
-    public String query3 = "";
-    public String query4 = "";
+    public String query1 = "ALTER TABLE movies ADD watchCount int";
+    public String query2 = "ALTER TABLE movies ADD isTop tinyint DEFAULT 0";
+    public String query3 = "ALTER TABLE cinemas ADD openTime time";
+    public String query4 = "ALTER TABLE cinemas ADD closeTime time";
     public String query5 = "";
 }
